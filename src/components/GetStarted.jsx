4bb4953b83c80b09399bb1068b145ab7f4d89642
rohan209547mourya <rohan.209547@gmail.com>
2023-01-22 20:47:12 +0530

    hero section done

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import ArrowUp from '../assets/arrow-up.svg'
 
 const GetStarted = () => {
     return (
-        <div>GetStarted</div>
+        <div className='flex justify-center items-center w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer'>
+
+            <div className="flex justify-center items-center flex-col bg-primary w-[100%] h-[100%] rounded-full">
+
+                <div className='flex justify-center items-start flex-row'>
+
+                    <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
+
+                        <span className='text-gradient'>Get</span>
+                    </p>
+
+                    <img src={ArrowUp} alt="arrowup" className='w-[23px] h-[23px] object-contain' />
+                </div>
+                <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+
+                    <span className='text-gradient'>Started</span>
+                </p>
+            </div>
+
+        </div>
     )
 }
 
